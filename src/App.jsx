0c5a6e88674eafd557798b16fd2d0417594310e2
Eric Jimenez <ericfjmnz@gmail.com>
2025-06-13 20:57:10 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -654,20 +654,20 @@ function HomePage() {
                 </div>
             </ContentSection>
             
-            {featuredPlaylistsUrl && 
-                <ContentSection title={featuredPlaylistsData?.message || "Featured Playlists"} loading={playlistsLoading} error={playlistsError}>
-                     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-                        {featuredPlaylistsData?.playlists.items.map(playlist => (
-                           <PlaylistCard 
-                                key={playlist.id} 
-                                imageUrl={playlist.images[0]?.url}
-                                title={playlist.name}
-                                subtitle={playlist.description.replace(/<[^>]*>?/gm, '')}
-                            />
-                        ))}
-                     </div>
-                </ContentSection>
-            }
+            // {featuredPlaylistsUrl && 
+            //     <ContentSection title={featuredPlaylistsData?.message || "Featured Playlists"} loading={playlistsLoading} error={playlistsError}>
+            //          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+            //             {featuredPlaylistsData?.playlists.items.map(playlist => (
+            //                <PlaylistCard 
+            //                     key={playlist.id} 
+            //                     imageUrl={playlist.images[0]?.url}
+            //                     title={playlist.name}
+            //                     subtitle={playlist.description.replace(/<[^>]*>?/gm, '')}
+            //                 />
+            //             ))}
+            //          </div>
+            //     </ContentSection>
+            // }
 
             <ContentSection title="Your Top Artists" loading={artistsLoading} error={artistsError}>
                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
@@ -801,7 +801,7 @@ function PlaylistCreator() {
         setIsWqxrLoading(true);
         setError('');
         setCreatedPlaylist(null);
-        setStatus('Requesting playlist from proxy server...');
+        setStatus('Starting WQXR playlist creation...');
 
         try {
             const { year, month, day } = getYesterdayDateParts();
@@ -1154,7 +1154,6 @@ function DeleteConfirmationModal({ playlist, onClose }) {
         </div>
     );
 }
-
 
 
 
